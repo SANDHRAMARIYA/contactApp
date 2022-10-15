fc4bf4de87c07eee44307b3c7e6aa925b5d23328
SANDHRAMARIYA <sandhramariya09@gmail.com>
2022-10-15 11:29:16 +0530

    view

---
+++ src/app/viewcontact/viewcontact.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-viewcontact',
+  templateUrl: './viewcontact.component.html',
+  styleUrls: ['./viewcontact.component.css']
+})
+export class ViewcontactComponent implements OnInit {
+
+  constructor() { }
+
+
+
+
+contactData:any={}
+
+  ngOnInit(): void {
+  }
+
+}
